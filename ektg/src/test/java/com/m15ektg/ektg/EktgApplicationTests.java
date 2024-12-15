4b6c4bb698e16353b0343928a1555733973460ed
Mike <mpetrenko.m15ios@gmail.com>
2024-12-15 19:54:43 +0300

    init spring boot

---
+++ ektg/src/test/java/com/m15ektg/ektg/EktgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.m15ektg.ektg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EktgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
