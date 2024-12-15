96984fa89d60c022e960c79eee49be14167a1beb
Mike <mpetrenko.m15ios@gmail.com>
2024-12-15 20:31:59 +0300

    executable version

--- ektg/src/main/java/com/m15ektg/ektg/EktgApplication.java
+++ ektg/src/main/java/com/m15ektg/ektg/EktgApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class EktgApplication {
 
 	public static void main(String[] args) {
+		System.out.println("trololo");
 		SpringApplication.run(EktgApplication.class, args);
 	}
 
