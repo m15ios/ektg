d168f077332fd1e127282c22a92458ecc544b29c
Mike <mpetrenko.m15ios@gmail.com>
2024-12-20 20:14:03 +0300

    normal run without liqu and pg

--- ektg/src/main/java/e_katalog/com/ru/ektg2/Ektg2Application.java
+++ ektg/src/main/java/e_katalog/com/ru/ektg2/Ektg2Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Ektg2Application {
 
 	public static void main(String[] args) {
+		System.out.println("\n\n\nAPP STARTING\n\n\n");
 		SpringApplication.run(Ektg2Application.class, args);
 	}
 
