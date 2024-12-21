e4ef1c073aaacd2af7f2d68995b0e231c8a5285a
Mike <mpetrenko.m15ios@gmail.com>
2024-12-21 20:21:06 +0300

    sql fixed

--- ektg/src/main/resources/db/changelog/changesets/release-1.0.0/create-tables.sql
+++ ektg/src/main/resources/db/changelog/changesets/release-1.0.0/create-tables.sql
@@ -1,57 +1,32 @@
---liquibase formatted sql
---changeset ektg:1
-
-/*==============================================================*/
-/* Table: profile                                               */
-/*==============================================================*/
-
-create table public.profile
+create table profile
 (
-    id                   bigint not null,
-                         constraint pk_profile
-                         primary key,
+    id                   bigint not null primary key,
     tg_id                bigint not null,
     profile_status       smallint,  
     create_date          date,
     create_ip_address    varchar(255)
 );
 
-/* comment on table public.profile is 'Учетная запись пользователя';       */
-/* comment on column public.profile.id is 'Порядковый номер пользователя'; */
-
-/*==============================================================*/
-/* Index: profile                                               */
-/*==============================================================*/
-
 create index profile_tg_index
     on profile (tg_id);
 
 
-/*==============================================================*/
-/* Table: offer                                                 */
-/*==============================================================*/
-
-create table public.offer
+create table offer
 (
-    id                   bigint not null,
-                         constraint pk_offer
-                         primary key,
+    id                   bigint not null primary key,
     profile_id           bigint not null,
     offer_code           varchar(255),
     offer_status         smallint,
     offer_price          numeric(18, 10),
-    offer_description    text
+    offer_description    text,
     create_date          date,
     create_ip_address    varchar(255)
 );
 
-/*==============================================================*/
-/* Index: offer                                                 */
-/*==============================================================*/
 
 create index offer_profile_index
-    on profile (profile_id);
+    on offer (profile_id);
 
 create index offer_code_index
-    on profile (offer_code);
+    on offer (offer_code);
 
